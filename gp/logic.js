b10ad52a68dc44bda24e0530ca60ab709b8a796d
tejasswii <158274770+tejasswii@users.noreply.github.com>
2024-02-26 14:58:50 +0530

    Add files via upload
    work in progress

---
+++ gp/logic.js
@@ -0,0 +1,9 @@
+
+const typed = new Typed('#typed-text', {
+    strings: ['frontend developer', 'backend developer', 'web developer'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+    
+  });
